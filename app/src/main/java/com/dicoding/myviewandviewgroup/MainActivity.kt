11327090f1a36bf7c16d8af634ffeec4f6f03f0d
keerrrrl <dwikyalfira1@gmail.com>
2023-08-31 21:00:06 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/myviewandviewgroup/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.dicoding.myviewandviewgroup
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
+}
